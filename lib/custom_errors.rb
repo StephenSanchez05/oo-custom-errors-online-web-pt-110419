0645c5fdd742f70d8d347ca7b0e3641100562d0d
Stephen Sanchez <stephensanchez05@gmail.com>
2020-01-04 23:10:25 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,13 +5,17 @@ class Person
     @name = name
   end
 
-  def get_married(person)
+ def get_married(person)
     self.partner = person
     if person.class != Person
-      raise PartnerError
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
     else
-    person.partner = self
-  end
+      person.partner = self
+    end
   end
 
 class PartnerError < StandardError
